ceaa48231870e8f11f7efc7f1c76c4c10ccc7cd0
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-09-27 18:22:21 +0100

    Initial Commit

---
+++ GoFramework/GoFramework.h
@@ -0,0 +1,19 @@
+//
+//  GoFramework.h
+//  GoFramework
+//
+//  Created by Ondrej Rafaj on 27/09/2016.
+//  Copyright © 2016 Ondrej Rafaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoFramework.
+FOUNDATION_EXPORT double GoFrameworkVersionNumber;
+
+//! Project version string for GoFramework.
+FOUNDATION_EXPORT const unsigned char GoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoFramework/PublicHeader.h>
+
+
