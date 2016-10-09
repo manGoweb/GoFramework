1015d33314166f3c753d40bc22ee6269b1af473d
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-10-09 21:56:15 +0100

    adding hex colors

--- GoFramework/UIColor+Go.swift
+++ GoFramework/UIColor+Go.swift
@@ -40,13 +40,42 @@ public struct GoUIColor {
         
     }
     
-    public static var random: UIColor {
+    public var random: UIColor {
         get {
             let randomRed: CGFloat = CGFloat(drand48())
             let randomGreen: CGFloat = CGFloat(drand48())
             let randomBlue: CGFloat = CGFloat(drand48())
             return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
         }
+    }
+    
+    public func hex(_ hexString: String) -> UIColor? {
+        let r, g, b, a: CGFloat
+        // TODO: Make it take color code without the #
+        if hexString.hasPrefix("#") {
+            let start = hexString.index(hexString.startIndex, offsetBy: 1)
+            let hexColor = hexString.substring(from: start)
+            
+            if hexColor.characters.count >= 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    if hexColor.characters.count >= 6 {
+                        a = CGFloat(hexNumber & 0x000000ff) / 255
+                    }
+                    else {
+                        a = 1
+                    }
+                    
+                    return UIColor.init(red: r, green: g, blue: b, alpha: a)
+                }
+            }
+        }
+        return nil
     }
     
 }
