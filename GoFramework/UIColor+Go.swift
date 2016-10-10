ecbae48f46afb6d3d8f1bec09bc5001037e1ff61
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-10-10 21:20:54 +0100

    color extensions

--- GoFramework/UIColor+Go.swift
+++ GoFramework/UIColor+Go.swift
@@ -49,33 +49,56 @@ public struct GoUIColor {
         }
     }
     
-    public func hex(_ hexString: String) -> UIColor? {
-        let r, g, b, a: CGFloat
-        // TODO: Make it take color code without the #
+    public func rgb(red: Int, green: Int, blue: Int) -> UIColor {
+        #if DEBUG
+            assert(red >= 0 && red <= 255, "Invalid red component")
+            assert(green >= 0 && green <= 255, "Invalid green component")
+            assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        #endif
+        return UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    public func hex(hex: Int) -> UIColor? {
+        return self.rgb(red:(hex >> 16) & 0xff, green:(hex >> 8) & 0xff, blue:hex & 0xff)
+    }
+    
+    public func hex(string hexString: String) -> UIColor? {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+        
         if hexString.hasPrefix("#") {
-            let start = hexString.index(hexString.startIndex, offsetBy: 1)
-            let hexColor = hexString.substring(from: start)
-            
-            if hexColor.characters.count >= 6 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    if hexColor.characters.count >= 6 {
-                        a = CGFloat(hexNumber & 0x000000ff) / 255
-                    }
-                    else {
-                        a = 1
-                    }
-                    
-                    return UIColor.init(red: r, green: g, blue: b, alpha: a)
+            let index = hexString.index(hexString.startIndex, offsetBy: 1)
+            let hex = hexString.substring(from: index)
+            let scanner = Scanner(string: hex)
+            var hexValue: CUnsignedLongLong = 0
+            if scanner.scanHexInt64(&hexValue) {
+                switch (hex.characters.count) {
+                case 3:
+                    red = CGFloat((hexValue & 0xF00) >> 8) / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4) / 15.0
+                    blue = CGFloat(hexValue & 0x00F) / 15.0
+                case 4:
+                    red = CGFloat((hexValue & 0xF000) >> 12) / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8) / 15.0
+                    blue = CGFloat((hexValue & 0x00F0) >> 4) / 15.0
+                    alpha = CGFloat(hexValue & 0x000F) / 15.0
+                case 6:
+                    red = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
+                    blue = CGFloat(hexValue & 0x0000FF) / 255.0
+                case 8:
+                    red = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue = CGFloat((hexValue & 0x0000FF00) >> 8) / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF) / 255.0
+                default:
+                    break;
                 }
             }
         }
-        return nil
+        return UIColor(red:red, green:green, blue:blue, alpha:alpha)
     }
     
 }
