4f318b59a317bc1d0cdab00f8db745c60e1f5090
vytick <vytick@gmail.com>
2016-10-11 14:17:20 +0200

    updated GoColor.hex(), added String extension for localization

---
+++ GoFramework/String+Go.swift
@@ -0,0 +1,32 @@
+//
+//  String+Go.swift
+//  GoFramework
+//
+//  Created by Martin Vytrhlík on 11/10/2016.
+//  Copyright © 2016 Ondrej Rafaj. All rights reserved.
+//
+
+import Foundation
+
+
+public extension String {
+    
+    public var go: GoString {
+        get {
+            return GoString(string: self)
+        }
+    }
+}
+
+public struct GoString {
+    
+    private var string: String
+    
+    init(string: String) {
+        self.string = string
+    }
+    
+    public var localized: String {
+        return NSLocalizedString(string, comment: "")
+    }
+}
