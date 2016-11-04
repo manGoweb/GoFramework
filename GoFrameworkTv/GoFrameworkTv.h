18b29ded08e88875518731752c663f734c9ac904
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-11-04 11:48:42 +0000

    tv os support

---
+++ GoFrameworkTv/GoFrameworkTv.h
@@ -0,0 +1,19 @@
+//
+//  GoFrameworkTv.h
+//  GoFrameworkTv
+//
+//  Created by Ondrej Rafaj on 04/11/2016.
+//  Copyright © 2016 Ondrej Rafaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoFrameworkTv.
+FOUNDATION_EXPORT double GoFrameworkTvVersionNumber;
+
+//! Project version string for GoFrameworkTv.
+FOUNDATION_EXPORT const unsigned char GoFrameworkTvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoFrameworkTv/PublicHeader.h>
+
+
